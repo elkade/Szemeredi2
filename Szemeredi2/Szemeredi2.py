ed9404ce7538a5eb327b869bca6afafcfbf9756d
Łukasz <draganl@student.mi.pw.edu.pl>
2016-12-09 09:26:46 +0100

    human vs computer

--- Szemeredi2/Szemeredi2.py
+++ Szemeredi2/Szemeredi2.py
@@ -12,7 +12,7 @@ finder  = LongestSubProgressionFinder()
 game = Game(finder)
 
 human = HumanPlayer(Number.selected_by_player_a)
-human = ComputerPlayer(finder, Number.selected_by_player_a)
+#human = ComputerPlayer(finder, Number.selected_by_player_a)
 computer = ComputerPlayer(finder, Number.selected_by_player_b)
 
 game_master = GameMaster(human, computer)
